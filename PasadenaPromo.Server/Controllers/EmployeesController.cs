ade4093150846fb017d49f1308d48e657902a500
FatAlien <vladislav.chigrinov@gmail.com>
2024-08-14 14:30:26 +0300

    Add bruno requests

---
+++ PasadenaPromo.Server/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasadenaPromo.Server.Controllers
+{
+    namespace PasadenaPromo.Server.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class EmployeeProfileController(ApplicationContext db) : ControllerBase
+        {
+            readonly private ApplicationContext _db = db;
+
+        }
+    }
+}
